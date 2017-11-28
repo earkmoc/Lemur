fac6673bd4658de218ab26001816bc2c8ab24513
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2017-11-28 16:12:17 +0100

    Drukowanie pola UWAGI na ewidencji czasu pracy

--- Lemur/Pracownicy/Tabela/ecpAbsencje.php
+++ Lemur/Pracownicy/Tabela/ecpAbsencje.php
@@ -18,6 +18,18 @@ while($r=mysqli_fetch_array($w))
 {
 	$wynik[]=$r;
 }
+
+$q="
+select UWAGI
+  from pracownicy
+ where ID='$id'
+";
+$w=mysqli_query($link,$q); if(mysqli_error($link)) {echo "Error: ".$_SESSION['error'].=mysqli_error($link)." in $q";}
+while($r=mysqli_fetch_array($w))
+{
+	$wynik[]='UWAGI: '.$r[0];
+}
+
 //print_r($wynik);die;
 if($wynik=json_encode($wynik))
 {
