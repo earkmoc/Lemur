a2347e7eb77d836fe6c7ccf45daa478d456e366b
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2018-03-15 21:33:03 +0100

    Usuwanie zbędnych klientów

--- Batory/Dokumenty/Tabela/view.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('input[name=okres]').focus();
-	$('input[name=miasto]').focus();
-	$('input[name=typZT]').focus();
-	$('input[name=data1]').focus();
-	$('input[name=tytul]').focus();
-});
--- Batory/Rejestry/Tabela/view.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('input[name=data1]').focus();
-	$('input[name=tytul]').focus();
-	$('input[name=rok]').focus();
-	$('input[name=okres]').focus();
-});
