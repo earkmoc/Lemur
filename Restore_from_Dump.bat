abdd1847c147ea68d0de013a7a37982d958facd8
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2018-03-17 16:08:19 +0100

    Zmiana hasła

--- Restore_from_Dump.bat
+++ Restore_from_Dump.bat
@@ -7,7 +7,7 @@ SET ext=Dump
 SET sql=%baza%_%ext%.sql
 SET arch=%baza%_%ext%.7z
 
-"C:\Program Files\7-Zip\7z.exe" x %arch% -pProsper!@#223#@!
+"C:\Program Files\7-Zip\7z.exe" x %arch% -pEla!@#223#@!
 C:\wamp\bin\mysql\mysql5.7.14\bin\mysql.exe -u%u% -p%p% -e "drop database %1";
 C:\wamp\bin\mysql\mysql5.7.14\bin\mysql.exe -u%u% -p%p% -e "create database %1";
 C:\wamp\bin\mysql\mysql5.7.14\bin\mysql.exe -u%u% -p%p% %baza% < %sql%
