b203f14f4e6afad3a26003084001bf6671442d3b
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2018-03-17 16:59:28 +0100

    usuwanie zapisów z zakładek przy usuwaniu dokumentu

--- Lemur/Dokumenty/Tabela/usun.php
+++ Lemur/Dokumenty/Tabela/usun.php
@@ -3,7 +3,7 @@
 require("setup.php");
 require("{$_SERVER['DOCUMENT_ROOT']}/Lemur2/saveTablePosition.php");
 
-$readonly=( ((mysqli_fetch_row(mysqli_query($link, "select GDZIE from dokumenty where ID=$id"))[0])=='ksiÃªgi')
+$readonly=( ((mysqli_fetch_row(mysqli_query($link, "select GDZIE from dokumenty where ID=$id"))[0])=='ksiêgi')
 			||( ($ido==0)
 			  &&($id)
 			  &&((mysqli_fetch_row(mysqli_query($link, "select KTO from dokumenty where ID=$id"))[0])!=$ido)
@@ -28,9 +28,11 @@ else
 		header("location:../Tabela");
 	}
 
-	mysqli_query($link, $q="delete from dokumentk where ID_D='$id'");if (mysqli_error($link)) {die(mysqli_error($link).'<br>'.$q);}
-	mysqli_query($link, $q="delete from dokumentm where ID_D='$id'");if (mysqli_error($link)) {die(mysqli_error($link).'<br>'.$q);}
-	mysqli_query($link, $q="delete from dokumentr where ID_D='$id'");if (mysqli_error($link)) {die(mysqli_error($link).'<br>'.$q);}
-	mysqli_query($link, $q="delete from kpr       where ID_D='$id'");if (mysqli_error($link)) {die(mysqli_error($link).'<br>'.$q);}
-	mysqli_query($link, $q="delete from ewidsprz  where ID_D='$id'");if (mysqli_error($link)) {die(mysqli_error($link).'<br>'.$q);}
+	mysqli_query($link, $q="delete from dokumentk where ID_D='$id'");
+	mysqli_query($link, $q="delete from dokumentm where ID_D='$id'");
+	mysqli_query($link, $q="delete from dokumentr where ID_D='$id'");
+	mysqli_query($link, $q="delete from kpr       where ID_D='$id'");
+	mysqli_query($link, $q="delete from ewidsprz  where ID_D='$id'");
+	mysqli_query($link, $q="delete from ewidprzeb where ID_D='$id'");
+	mysqli_query($link, $q="delete from ewidwypo  where ID_D='$id'");
 }
