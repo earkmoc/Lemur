9aa80f8d66dda5010fce5c91c2b2bd6d879c9f28
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2018-04-17 11:07:07 +0200

    Controller basics

---
+++ zend-app/module/BookList/src/BookList/Controller/BookController.php
@@ -0,0 +1,24 @@
+<?php
+namespace BookList\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class BookController extends AbstractActionController
+{
+	public function indexAction()
+	{
+		
+	}
+	public function addAction()
+	{
+		
+	}
+	public function editAction()
+	{
+		
+	}
+	public function deleteAction()
+	{
+		
+	}
+}
