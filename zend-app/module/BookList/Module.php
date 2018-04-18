455530577043e03e921d302a3f76c83a876d2afb
Arkadiusz Moch <Arkadiusz.Moch@gmail.com>
2018-04-18 10:18:14 +0200

    Configuring TableGateway

--- zend-app/module/BookList/Module.php
+++ zend-app/module/BookList/Module.php
@@ -2,6 +2,11 @@
 
 namespace BookList;
 
+use BookList\Model\Book;
+use BookList\Model\BookTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
 class Module
 {
 	public function getAutoloaderConfig()
@@ -21,5 +26,26 @@ class Module
 	public function getConfig()
 	{
 		return include __DIR__.'/config/module.config.php';
+	}
+	
+	public function getServiceConfig()
+	{
+		return array(
+			'factories'=>array(
+				'BookList\Model\BookTable'=>function($sm)
+				{
+					$tableGateway=$sm->get('BookTableGateway');
+					$table=new BookTable($tableGateway);
+					return $table;
+				},
+				'BookTableGateway'=>function($sm)
+				{
+					$dbAdapter=$sm->get('Zend\Db\Adapter\Adapter');
+					$resultSetPrototype=new ResultSet();
+					$resultSetPrototype->setArrayObjectPrototype(new Book());
+					return new TableGateway('book',$dbAdapter,null,$resultSetPrototype);
+				},
+			),
+		);
 	}
 }
